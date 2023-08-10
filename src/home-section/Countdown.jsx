10d154051ea67b2a70bd677bedb6b0d7d5a8736a
tamuno777 <davisbeulah07@gmail.com>
2023-08-10 07:25:10 -0700

    count

--- src/home-section/Countdown.jsx
+++ src/home-section/Countdown.jsx
@@ -4,22 +4,22 @@ export default function Countdown() {
   return (
     <div className='d-flex py-4  countdown-container justify-content-center'>
       <div className='mx-3 countdown-content'>
-      <h1>230</h1>
+      <h1>3</h1>
       <p><small><strong>Days</strong></small></p>
       </div>
       
       <div className='mx-3 countdown-content'>
-      <h1>16</h1>
+      <h1>72</h1>
       <p><small><strong>Hours</strong></small></p>
       </div>
       
       <div className='mx-3 countdown-content'>
-      <h1>14</h1>
+      <h1>4320</h1>
       <p><small><strong>Minutes</strong></small></p>
       </div>
       
       <div className='mx-3 countdown-content'>
-      <h1>20</h1>
+      <h1>259200</h1>
       <p><small><strong>Seconds</strong></small></p>
       </div>
     </div>
